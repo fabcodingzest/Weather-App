26110a0f8310ea763cbd7ad648d46faab79412a2
fabcodingzest <fabrizvi786@gmail.com>
2020-05-06 16:22:12 +0530

    Added API KEY

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const chk = document.getElementById('chk');
+require('dotenv').config();
+
+const api_key = process.env.API_KEY;
 
 chk.addEventListener('change', () => {
   document.body.classList.toggle('light-theme');
