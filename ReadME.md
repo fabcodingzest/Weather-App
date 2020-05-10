c366c3eae289321e002006dc89265f009c7e9245
fabcodingzest <fabrizvi786@gmail.com>
2020-05-10 22:36:44 +0530

    Added comments and ReadME File

---
+++ ReadME.md
@@ -0,0 +1,16 @@
+# Weather App made with Vanilla JavaScript
+***
+> ## [Check it out live here](https://fab-breeze.netlify.app/)
+***
+
+** I made this simple weather app to practice my JavaScript skills. \ I used plain HTML, CSS and JavaScript for the project. \  I also used webpack for minifying the code and to access environment variables I used webpack-dotenv npm package using webpack as well **
+
+* Feel free to Fork the repo and play with the code *
+
+To run this on your local machine you need run the command `npm start`
+Don't forget to add a .env file with the following code
+
+```
+API_KEY = (your api_key from OpenWeathermap.org/api)
+PROXY=https://cors-anywhere.herokuapp.com/`
+```
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './main.css';
+// Selecting Theme toggle, search Form and div where fetched API results are to be displayed from DOM
 const chk = document.querySelector('.label');
 const searchInput = document.querySelector('.search__query--input');
 const searchedRes = document.querySelector('.searched__response');
+
+// Icons to display according to the icon-ID recieved by API
 const iconData = {
   '01d': { icon: '<i class="fas fa-sun"></i>' },
   '01n': { icon: '<i class="fas fa-sun"></i>'},
@@ -22,11 +25,13 @@ const iconData = {
   '50d': { icon: '<i class="fas fa-smog"></i>' },
   '50n': { icon: '<i class="fas fa-smog"></i>'},
 }
+// Accessing API from environment variables
 const api_key = process.env.API_KEY;
 const api = {
   key: api_key,
   baseUrl: "api.openweathermap.org/data/2.5/"
 }
+// Function to display No results found when the search query is invalid
 function CheckError (response) {
   if (response.status >= 200 && response.status <= 299) {
     return response.json();
@@ -36,6 +41,7 @@ function CheckError (response) {
     throw Error(response.statusText);
   }
 }
+// Asynchronous function to display loader till it fetches data from API acc to the input
 async function getSearchResults (query) {
   const loading = `<div class="box">
                       <div class="loader"></div>
@@ -45,6 +51,7 @@ async function getSearchResults (query) {
   let data = await CheckError(response)
   return data;
 }
+// A date function to display date in 'Day, date month year' format from date recieved from API
 function datebuilder (d) {
   const months = ['January','February','March','April','May','June','July','August','September','October','November','December'
   ];
@@ -56,7 +63,7 @@ function datebuilder (d) {
   const dayName = days[ d.getDay() ];
   return`${dayName}, ${date} ${monthName} ${year}`;
 }
-
+// Results fetched from the API and injecting them into DOM
 function displayResults (weather) {
   let now = new Date();
   const dataRes = `
@@ -81,21 +88,24 @@ function displayResults (weather) {
   `
   searchedRes.innerHTML = dataRes;
 }
+// Search function when enter/return is pressed
 function search (e) {
   if (e.keyCode === 13)
   {
     e.preventDefault();
+    // Get results from api asynchronously and catching any error if it exists 
     getSearchResults(searchInput.value).then(displayResults).catch(error=>console.log(error));
     searchInput.value = '';
+    // Remove the keyboard after pressing enter on mobile devices
     searchInput.blur();
-    console.log(searchInput.value);
   }
 }
 
-
+// Event Listener for dark-light theme toggle
 chk.addEventListener('click', () => {
   console.log(document.querySelector('.ball').classList.toggle('toggle'))
   document.body.classList.toggle('light-theme');
 });
+// Event listener for search 
 searchInput.addEventListener('keypress', search);
 
